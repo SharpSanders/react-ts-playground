0d5f3390d85d39d4c90e7916f464d37d99a05160
Trevyn Sanders <trevynsanders08@gmail.com>
2026-01-01 18:05:25 -0600

    chore: initialize react + typescript workspace

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>React + TypeScript Playground</h1>
+      <p>January 2026 – Foundations</p>
+    </div>
+  );
+}
+
+export default App;
+
