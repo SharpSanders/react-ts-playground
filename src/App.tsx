420ea711283fc03afc8c08fb2f66ffaba05fc384
Trevyn Sanders <trevynsanders08@gmail.com>
2026-01-08 16:47:33 -0600

    docs: apply react ui composition concepts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import { Header } from "./components/Header";
 import { Counter } from "./components/Counter";
 
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>React + TypeScript Playground</h1>
-      <p>January 2026 – Foundations</p>
-
+      <Header />
       <Counter initialCount={5} />
     </div>
   );
